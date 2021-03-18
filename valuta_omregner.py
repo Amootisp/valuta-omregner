f6281a6644501dd6ef60a010fb20773f7df8a297
Cedrik Pedersen <cedrikpedersen@gmail.com>
2021-03-18 09:32:53 +0100

    Add some text updating

--- valuta_omregner.py
+++ valuta_omregner.py
@@ -24,10 +24,11 @@ for tag in root_node.findall('dailyrates/currency'):
     
 
 # Define the window's contents
-layout = [  [sg.Combo([country_codes[0], country_codes[1], country_codes[2], country_codes[3], country_codes[4], country_codes[5], country_codes[6], country_codes[7], country_codes[8], country_codes[9], country_codes[10], country_codes[11], country_codes[12], country_codes[13], country_codes[14], country_codes[15], country_codes[16], country_codes[17], country_codes[18], country_codes[19], country_codes[20], country_codes[21], country_codes[22], country_codes[23], country_codes[24], country_codes[25], country_codes[26], country_codes[27], country_codes[28], country_codes[29], country_codes[30], country_codes[31], country_codes[32]], enable_events=True, key='code'),
+layout = [  [sg.Text('Valuta 1', key='-VALUTA1-'), sg.Text('Valuta 2', key='-VALUTA2-')],
+            [sg.Combo([country_codes[0], country_codes[1], country_codes[2], country_codes[3], country_codes[4], country_codes[5], country_codes[6], country_codes[7], country_codes[8], country_codes[9], country_codes[10], country_codes[11], country_codes[12], country_codes[13], country_codes[14], country_codes[15], country_codes[16], country_codes[17], country_codes[18], country_codes[19], country_codes[20], country_codes[21], country_codes[22], country_codes[23], country_codes[24], country_codes[25], country_codes[26], country_codes[27], country_codes[28], country_codes[29], country_codes[30], country_codes[31], country_codes[32]], enable_events=True, key='code'),
             sg.Combo([country_codes[0], country_codes[1], country_codes[2], country_codes[3], country_codes[4], country_codes[5], country_codes[6], country_codes[7], country_codes[8], country_codes[9], country_codes[10], country_codes[11], country_codes[12], country_codes[13], country_codes[14], country_codes[15], country_codes[16], country_codes[17], country_codes[18], country_codes[19], country_codes[20], country_codes[21], country_codes[22], country_codes[23], country_codes[24], country_codes[25], country_codes[26], country_codes[27], country_codes[28], country_codes[29], country_codes[30], country_codes[31], country_codes[32]], enable_events=True, key='combo')],     # Part 2 - The Layout
             [sg.Input('Skriv tal ellers crash', key='input')],
-            [sg.Text('My layout', key='-TEXT-')],
+            [sg.Text('Resultalt', key='-TEXT-')],
             [sg.Button('Ok')] ]
 
 # Create the window
@@ -49,12 +50,14 @@ while True:
             convert2 = rates[int(num2)]
             calnum1 = convert1.replace(',','.')
             calnum2 = convert2.replace(',','.')
+            window['-VALUTA1-'].update(country_names[int(num1)])
+            window['-VALUTA2-'].update(country_names[int(num2)])
         except:
             sg.popup('Du skal huske at vælge country codes')
         try:
             window['-TEXT-'].update((float(calnum1) / 100) * (100 / float(calnum2)) * float(values['input']))
         except:
-            sg.popup('dit input skal være et tal')
+            sg.popup('dit input skal være et tal med mindre der var en tidligere fejl')
         
 
 # Finish up by removing from the screen
